66357c21239375d1f40de11f4d043c610710f5db
stevemwangi <sttevemwangi@gmail.com>
2020-06-30 11:39:36 +0300

    broken app into smaller components and styled the menu item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'; 
-import './homepage.styles.scss';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
